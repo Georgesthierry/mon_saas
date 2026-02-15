be492944214735d42ecdc2acfe75faf315cf9c95
Georgesthierry <thierrygeorgeskouassi@gmail.com>
2026-02-15 17:55:08 +0000

    Initial project structure: Django + Flutter + Docker

---
+++ backend/config/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.http import JsonResponse
+from django.urls import path
+
+def home(request):
+    return JsonResponse({"message": "Django API is working 🚀"})
+
+urlpatterns = [
+    path("", home),
+    path("admin/", admin.site.urls),
+]
